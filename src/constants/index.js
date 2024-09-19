0ca67694d20e19d5fdf360cedc6c058faed359d4
Gaurav Kumar <gk064577@gmail.com>
2024-09-20 02:35:09 +0530

    completed project section

--- src/constants/index.js
+++ src/constants/index.js
@@ -58,11 +58,11 @@ export const navLinks = [
   
   export const myProjects = [
     {
-      title: 'Podcastr - AI Podcast Platform',
-      desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+      title: 'Arnavs Blog - A Chatting Platform',
+      desc: 'Arnavs Blog is a revolutionary Software-as-a-Service platform that transforms the way of chatting. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
       subdesc:
-        'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-      href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+        'Built as a unique Software-as-a-Service app with React.js, Tailwind CSS, JavaScript, Socket.io, Express, Mongodb, Arnavs Blog is designed for optimal performance and scalability.',
+      href: 'https://github.com/Alex-The-Beast/mern-blog',
       texture: '/textures/project/project1.mp4',
       logo: '/assets/project-logo1.png',
       logoStyle: {
@@ -84,13 +84,13 @@ export const navLinks = [
         },
         {
           id: 3,
-          name: 'TypeScript',
-          path: '/assets/typescript.png',
+          name: 'JavaScript',
+          path: '/assets/js.png',
         },
         {
           id: 4,
-          name: 'Framer Motion',
-          path: '/assets/framer.png',
+          name: 'Node js',
+          path: '/assets/node.jpeg',
         },
       ],
     },
